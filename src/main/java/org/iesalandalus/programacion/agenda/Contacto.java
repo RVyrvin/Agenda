5ab7ee9c3e5445ec0490e8c107110eee9966a1e7
Hamster <Hamster@PC-6600>
2019-01-08 09:54:45 +0100

    3. Se ha creado clase Contacto con los atributos correspondientes

---
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -0,0 +1,15 @@
+package org.iesalandalus.programacion.agenda;
+
+public class Contacto {
+	
+	private static final String ER_TELEFONO = "exprecion regular";
+	private static final String ER_CORREO = "exprecion regular";
+	
+	private String nombre;
+	private String telefono;
+	private String correo;
+	
+	
+	
+
+}
