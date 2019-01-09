827aea3d370c0b5d881a650a01e3fc2d174b57f2
Hamster <Hamster@PC-6600>
2019-01-09 09:59:19 +0100

    8. se ha creado el constructor y los metodos get para la clase Agenda

--- src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -5,5 +5,20 @@ public class Agenda {
 	private static final int MAX_CONTACTOS = 5;	
 	private int numContactos;	
 	private Contacto[] contactos;
+	
+	
+	public Agenda () {
+		contactos = new Contacto[MAX_CONTACTOS];
+	}
+
+	public int getNumContactos() {
+		return numContactos;
+	}
+
+	public Contacto[] getContactos() {
+		return contactos;
+	}
+	
+	
 
 }
